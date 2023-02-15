de565a3637853b6ef0e7868daa8da4a8776e2ca3
Asif <sheikasifofficial@gmail.com>
2023-02-15 16:51:41 +0530

    Add widget extension

---
+++ CDWidget/CDWidget.swift
@@ -0,0 +1,68 @@
+//
+//  CDWidget.swift
+//  CDWidget
+//
+//  Created by Asif on 15/02/23.
+//
+
+import WidgetKit
+import SwiftUI
+import Intents
+
+struct Provider: IntentTimelineProvider {
+  func placeholder(in context: Context) -> SimpleEntry {
+    SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+  }
+  
+  func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    let entry = SimpleEntry(date: Date(), configuration: configuration)
+    completion(entry)
+  }
+  
+  func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    var entries: [SimpleEntry] = []
+    
+    // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+    let currentDate = Date()
+    for hourOffset in 0 ..< 5 {
+      let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+      let entry = SimpleEntry(date: entryDate, configuration: configuration)
+      entries.append(entry)
+    }
+    
+    let timeline = Timeline(entries: entries, policy: .atEnd)
+    completion(timeline)
+  }
+}
+
+struct SimpleEntry: TimelineEntry {
+  let date: Date
+  let configuration: ConfigurationIntent
+}
+
+struct CDWidgetEntryView : View {
+  var entry: Provider.Entry
+  
+  var body: some View {
+    Text(entry.date, style: .time)
+  }
+}
+
+struct CDWidget: Widget {
+  let kind: String = "CDWidget"
+  
+  var body: some WidgetConfiguration {
+    IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
+      CDWidgetEntryView(entry: entry)
+    }
+    .configurationDisplayName("My Widget")
+    .description("This is an example widget.")
+  }
+}
+
+struct CDWidget_Previews: PreviewProvider {
+  static var previews: some View {
+    CDWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+      .previewContext(WidgetPreviewContext(family: .systemSmall))
+  }
+}
