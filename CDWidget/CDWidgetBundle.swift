4f4699a09e229d177fa0d1f00a01493eb489ca53
Asif <sheikasifofficial@gmail.com>
2023-02-15 16:51:41 +0530

    Add widget extension

---
+++ CDWidget/CDWidgetBundle.swift
@@ -0,0 +1,19 @@
+//
+//  CDWidgetBundle.swift
+//  CDWidget
+//
+//  Created by Asif on 15/02/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CDWidgetBundle: WidgetBundle {
+  var body: some Widget {
+    CDWidget()
+    if #available(iOSApplicationExtension 16.1, *) {
+      CDWidgetLiveActivity()
+    }
+  }
+}
