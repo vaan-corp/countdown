660ae346e4116f817b5a6faef6c7566e80dfbd2a
Asif <sheikasifofficial@gmail.com>
2023-02-20 07:40:07 +0530

    Fix PR comments on widget-updates

--- CDWidget/SmallWidget/NoEventsView.swift
+++ CDWidget/SmallWidget/NoEventsView.swift
@@ -10,14 +10,20 @@ import SwiftUI
 struct NoEventsView: View {
   let kind: String
   var image: Image { kind == "favEvents" ? Image(systemName:"heart.fill") : Image("countdown")}
-  var text: String { kind == "favEvents" ? "No favorite events" : "No events"}
+  var favEventsText: String { "Open the app to add favorites." }
+  var upcomigEventsText: String { "Please check if your calendars have events and are enabled in the app's settings" }
+  var text: String { kind == "favEvents" ? favEventsText : upcomigEventsText}
   
   var body: some View {
-    VStack(spacing: 20){
+    VStack(spacing: 10){
       image
         .foregroundColor(.red)
         .imageScale(.large)
+        
       Text(text)
+        .font(.system(size: 14, weight: .bold))
+        .foregroundColor(.gray)
+        .padding([.leading, .trailing], 10)
     }
   }
 }
